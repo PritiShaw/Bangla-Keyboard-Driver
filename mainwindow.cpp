3e05188ec4722caaf384366ee97d42b30cdde892
Priti Shaw <34039705+PritiShaw@users.noreply.github.com>
2019-10-25 04:25:13 +0530

    Add mappings and manual links

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
-#include <QPlainTextEdit>
+#include <QDesktopServices>
+#include <QUrl>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -24,83 +26,124 @@ bool MainWindow::eventFilter(QObject *obj, QEvent *event)
 {
     if (event->type() == QEvent::KeyPress) {
         QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
-        if(unimap.find(keyEvent->text())!=unimap.end()/* && (keyEvent->modifiers() & Qt::ShiftModifier)*/){
-            ui->text->insertPlainText(unimap[keyEvent->text()].toUtf8());
-            return true;
+        if((keyEvent->modifiers() & Qt::ShiftModifier)){
+            if(unimap_shift.find(keyEvent->text())!=unimap_shift.end()){
+                ui->text->insertPlainText(unimap_shift[keyEvent->text()].toUtf8());
+                return true;
+            }
+            else{
+                return false;
+            }
         }
-        else
-            return false;
+        else{
+            if(unimap_unshift.find(keyEvent->text())!=unimap_unshift.end()){
+                ui->text->insertPlainText(unimap_unshift[keyEvent->text()].toUtf8());
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
     } else {
-        // standard event processing
         return QObject::eventFilter(obj, event);
     }
 }
 
 void MainWindow::createMap(){
-    unimap["."]="\u09F7";
-    unimap["-"]="\u0983";
-    unimap[","]="\u09B7";
-    unimap[";"]="\u099A";
-    unimap[";"]="\u099B";
-    unimap["'"]="\u099F";
-    unimap["'"]="\u09A0";
-    unimap["["]="\u09A1";
-    unimap["["]="\u09A2";
-    unimap["]"]="\u09BC";
-    unimap["]"]="\u099E";
-    unimap["/"]="\u09DF";
-    unimap["/"]="\u09AF";
-    unimap["="]="\u09C3";
-    unimap["="]="\u098B";
-    unimap["a"]="\u09CB";
-    unimap["A"]="\u0993";
-    unimap["b"]="\u09AC";
-    unimap["c"]="\u09AE";
-    unimap["C"]="\u09A3";
-    unimap["d"]="\u09CD";
-    unimap["D"]="\u0985";
-    unimap["e"]="\u09BE";
-    unimap["E"]="\u0986";
-    unimap["f"]="\u09BF";
-    unimap["F"]="\u0987";
-    unimap["g"]="\u09C1";
-    unimap["G"]="\u0989";
-    unimap["h"]="\u09AA";
-    unimap["H"]="\u09AB";
-    unimap["i"]="\u0997";
-    unimap["I"]="\u0998";
-    unimap["j"]="\u09B0";
-    unimap["k"]="\u0995";
-    unimap["K"]="\u0996";
-    unimap["l"]="\u09A4";
-    unimap["L"]="\u09A5";
-    unimap["m"]="\u09B8";
-    unimap["M"]="\u09B6";
-    unimap["n"]="\u09B2";
-    unimap["o"]="\u09A6";
-    unimap["O"]="\u09A7";
-    unimap["p"]="\u099C";
-    unimap["P"]="\u099D";
-    unimap["q"]="\u09CC";
-    unimap["Q"]="\u0994";
-    unimap["r"]="\u09C0";
-    unimap["R"]="\u0988";
-    unimap["s"]="\u09C7";
-    unimap["S"]="\u098F";
-    unimap["t"]="\u09C2";
-    unimap["T"]="\u098A";
-    unimap["u"]="\u09B9";
-    unimap["U"]="\u0999";
-    unimap["v"]="\u09A8";
-    unimap["w"]="\u09C8";
-    unimap["W"]="\u0990";
-    unimap["x"]="\u0982";
-    unimap["X"]="\u0981";
-    unimap["y"]="\u09AC";
-    unimap["Y"]="\u09AD";
+    // compile and run ./map/mapCodeGen.cpp file and paste output below
+    unimap_shift["_"]="\u0983";
+    unimap_shift["<"]="\u09B7";
+    unimap_shift[":"]="\u099B";
+    unimap_shift["\""]="\u09A0";
+    unimap_shift["{"]="\u09A2";
+    unimap_shift["}"]="\u099E";
+    unimap_shift["?"]="\u09AF";
+    unimap_shift["+"]="\u098B";
+    unimap_shift["$"]="\u09B0\u09CD";
+    unimap_shift["%"]="\u099C\u09CD\u099E";
+    unimap_shift["^"]="\u09A4\u09CD\u09B0";
+    unimap_shift["&"]="\u0995\u09CD\u09B7";
+    unimap_shift["*"]="\u09B6\u09CD\u09B0";
+    unimap_shift["A"]="\u0993";
+    unimap_shift["C"]="\u09A3";
+    unimap_shift["D"]="\u0985";
+    unimap_shift["E"]="\u0986";
+    unimap_shift["F"]="\u0987";
+    unimap_shift["G"]="\u0989";
+    unimap_shift["H"]="\u09AB";
+    unimap_shift["I"]="\u0998";
+    unimap_shift["K"]="\u0996";
+    unimap_shift["L"]="\u09A5";
+    unimap_shift["M"]="\u09B6";
+    unimap_shift["O"]="\u09A7";
+    unimap_shift["P"]="\u099D";
+    unimap_shift["Q"]="\u0994";
+    unimap_shift["R"]="\u0988";
+    unimap_shift["S"]="\u098F";
+    unimap_shift["T"]="\u098A";
+    unimap_shift["U"]="\u0999";
+    unimap_shift["W"]="\u0990";
+    unimap_shift["X"]="\u0981";
+    unimap_shift["Y"]="\u09AD";
+    unimap_unshift[";"]="\u099A";
+    unimap_unshift["'"]="\u099F";
+    unimap_unshift["["]="\u09A1";
+    unimap_unshift["]"]="\u09BC";
+    unimap_unshift["/"]="\u09DF";
+    unimap_unshift["="]="\u09C3";
+    unimap_unshift["0"]="\u09E6";
+    unimap_unshift["1"]="\u09E7";
+    unimap_unshift["2"]="\u09E8";
+    unimap_unshift["3"]="\u09E9";
+    unimap_unshift["4"]="\u09EA";
+    unimap_unshift["5"]="\u09EB";
+    unimap_unshift["6"]="\u09EC";
+    unimap_unshift["7"]="\u09ED";
+    unimap_unshift["8"]="\u09EE";
+    unimap_unshift["9"]="\u09EF";
+    unimap_unshift["a"]="\u09CB";
+    unimap_unshift["b"]="\u09AC";
+    unimap_unshift["c"]="\u09AE";
+    unimap_unshift["d"]="\u09CD";
+    unimap_unshift["e"]="\u09BE";
+    unimap_unshift["f"]="\u09BF";
+    unimap_unshift["g"]="\u09C1";
+    unimap_unshift["h"]="\u09AA";
+    unimap_unshift["i"]="\u0997";
+    unimap_unshift["j"]="\u09B0";
+    unimap_unshift["k"]="\u0995";
+    unimap_unshift["l"]="\u09A4";
+    unimap_unshift["m"]="\u09B8";
+    unimap_unshift["n"]="\u09B2";
+    unimap_unshift["o"]="\u09A6";
+    unimap_unshift["p"]="\u099C";
+    unimap_unshift["q"]="\u09CC";
+    unimap_unshift["r"]="\u09C0";
+    unimap_unshift["s"]="\u09C7";
+    unimap_unshift["t"]="\u09C2";
+    unimap_unshift["u"]="\u09B9";
+    unimap_unshift["v"]="\u09A8";
+    unimap_unshift["w"]="\u09C8";
+    unimap_unshift["x"]="\u0982";
+    unimap_unshift["y"]="\u09AC";
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionShow_Layout_triggered()
+{
+    QDesktopServices::openUrl(QUrl("https://i.imgur.com/cSqsENx.png", QUrl::TolerantMode));
+}
+
+void MainWindow::on_actionShow_Documentation_triggered()
+{
+    QDesktopServices::openUrl(QUrl("https://help.keyman.com/keyboard/bengali_inscript/1.0/chart.pdf", QUrl::TolerantMode));
+}
+
+void MainWindow::on_actionSystems_Lab_JU_BCSE_III_triggered()
+{
+    QDesktopServices::openUrl(QUrl("http://jaduniv.edu.in", QUrl::TolerantMode));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -22,9 +22,16 @@ public:
 private slots:
 
 
+    void on_actionShow_Layout_triggered();
+
+    void on_actionShow_Documentation_triggered();
+
+    void on_actionSystems_Lab_JU_BCSE_III_triggered();
+
 private:
     Ui::MainWindow *ui;
-    QMap<QString, QString> unimap;
+    QMap<QString, QString> unimap_shift;
+    QMap<QString, QString> unimap_unshift;
     void createMap();
 
 protected:
