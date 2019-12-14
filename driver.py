0ff0c5bbc0c9e3c4be26c23918fe84253a892195
Rohit Rajat Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2019-12-14 08:23:23 +0530

    Add files via upload

--- driver.py
+++ driver.py
@@ -2,24 +2,50 @@ import keyboard
 import codecs
 
 shift = False
+capsLock = False
+ctrl = False
+numLock = False
+
 shiftMap = {}
 unShiftMap = {}
 
-
 def remap_keyboard(src):
     def handler(event):
-        global shift
-        if (event.event_type == keyboard.KEY_DOWN):
-            if(shift and (src in shiftMap)):
-                keyboard.write(shiftMap[src])
-                shift = True
-            elif(src in unShiftMap):
-                keyboard.write(unShiftMap[src])
+        global ctrl,numLock,capsLock,shift
+        if(ctrl or (numLock and event.scan_code in [71,72,73,75,76,77,79,80,81,82])):
+            keyboard.send(event.scan_code)
+        elif((event.scan_code not in [53]) and ((event.scan_code not in [72,80,75,77]) or (event.scan_code in [53,72,80,75,77] and event.is_keypad))):
+            if (event.event_type == keyboard.KEY_DOWN):
+                if(not capsLock):
+                    if(shift and (src in shiftMap)):
+                        keyboard.write(shiftMap[src])
+                        shift = True
+                    elif(src in unShiftMap):
+                        keyboard.write(unShiftMap[src])
+                    else:
+                        keyboard.write(src)
+                else:
+                    if(shift and (src in unShiftMap)):
+                        keyboard.write(unShiftMap[src])
+                        shift = True
+                    elif(src in shiftMap):
+                        keyboard.write(shiftMap[src])
+                    else:
+                        keyboard.write(src)
+
             else:
-                keyboard.write(src)
+                keyboard.release(src)
         else:
-            keyboard.release(src)
-    keyboard.hook_key(src, handler, suppress=True)
+            keyboard.send(event.scan_code)
+    keyboard.on_press_key(src, handler, suppress=True)
+
+def capsLockHandler(e):
+    global capsLock
+    capsLock = not capsLock
+
+def numLockHandler(e):
+    global numLock
+    numLock = not numLock
 
 
 def shiftPressHandler(e):
@@ -32,8 +58,22 @@ def shiftReleaseHandler(e):
     shift = False
 
 
+def ctrlPressHandler(e):
+    global ctrl
+    ctrl = True
+
+
+def ctrlReleaseHandler(e):
+    global ctrl
+    ctrl = False
+
+
 keyboard.on_press_key('shift', shiftPressHandler)
 keyboard.on_release_key('shift', shiftReleaseHandler)
+keyboard.on_press_key('ctrl', ctrlPressHandler)
+keyboard.on_release_key('ctrl', ctrlReleaseHandler)
+keyboard.on_press_key('caps lock', capsLockHandler)
+keyboard.on_press_key('num lock', numLockHandler)
 
 f = codecs.open("shift.csv", mode="r", encoding="utf-8")
 for x in f:
